60131d502cdde74abe0dbbeab18b185c339bb0e1
Hossam Hassan <hossam.hassan9364@gmail.com>
2019-05-14 04:46:31 +0200

    first commit

---
+++ lib/gesturedetector.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+AudioCache player = new AudioCache();
+
+class GesDetector extends StatelessWidget {
+  GesDetector();
+
+  String _debugString = 'Origenele tekst';
+  bool pressed = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!pressed) {
+          player.play('ShhhSoundeffect.mp3');
+          pressed = true;
+        } else if (pressed) {
+          player.clear('ShhhSoundeffect.mp3');
+          pressed = false;
+          _debugString = 'onTap werkt!!!!!';
+          print(_debugString);
+        }
+      },
+      child: Image.asset('images/shhh.png'),
+    );
+  }
+}
